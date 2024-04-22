9d802c6268601ce70f0e96d99e39552c923272f8
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2024-04-22 16:55:33 -0300

    Update rockPaperScissors.js

--- 3. Functions/rockPaperScissors.js
+++ 3. Functions/rockPaperScissors.js
@@ -8,3 +8,70 @@ confident taking them to the real world. Why? Given a certain input you want to 
 If this than that.
 */
 
+console.log('hi');
+
+let userInput = 'bomb';
+
+const getUserChoice = userInput => {
+
+  userInput = userInput.toLowerCase();
+
+  if((userInput === 'rock') || (userInput === 'paper') || (userInput === 'scissors') || (userInput === 'bomb')) return userInput;
+  else return console.log('Error!');
+
+};
+
+
+function getComputerChoice(){
+
+  let temp = Math.random() * 3;
+  temp = Math.floor(temp);
+
+  switch(temp){
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      return console.log('Error');
+  }
+  
+}
+
+function determineWinner(userChoise, computerChoise){
+
+    if(userChoise === computerChoise) return console.log('TIE!');
+    else if(userChoise === 'rock'){
+        if(computerChoise === 'paper') return console.log('COMPUTER HAS WON!');
+        else return console.log('YOU ARE THE WINNER!');
+    }
+    else if(userChoise === 'paper'){
+        if(computerChoise === 'scissors') return console.log('COMPUTER HAS WON!');
+        else return console.log('YOU ARE THE WINNER!')
+    }
+    else if(userChoise === 'scissors'){
+        if(computerChoise === 'rock') return console.log('COMPUTER HAS WON!');
+        else console.log('YOU ARE THE WINNER!');
+    }
+    else return console.log('YOU ARE THE WINNER!');
+
+}
+
+function playGame(){
+
+    userInput = getUserChoice(userInput);
+    const computerChoise = getComputerChoice();
+
+    console.log(`Your choise is: ${userInput}`);
+    console.log(`Computer's choise is: ${computerChoise}`);
+
+    determineWinner(userInput, computerChoise);
+
+}
+
+playGame();
+
+
+
