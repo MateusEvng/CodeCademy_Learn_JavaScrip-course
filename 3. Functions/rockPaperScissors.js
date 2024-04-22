ea4a8e541386f1e17c913211dd6b71faac8f93c2
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2024-04-22 16:31:25 -0300

    Create rockPaperScissors.js

---
+++ 3. Functions/rockPaperScissors.js
@@ -0,0 +1,10 @@
+//LEARN JAVASCRIPT
+
+//Rock, Paper, or Scissors
+
+/*
+In this project, we’re going to practice conditionals in JavaScript so you can hone your skills and feel
+confident taking them to the real world. Why? Given a certain input you want to return a certain output. 
+If this than that.
+*/
+
