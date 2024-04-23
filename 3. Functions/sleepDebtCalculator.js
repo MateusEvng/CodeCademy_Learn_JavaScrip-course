02ddcad648708609b85e8401f6e24074acebd179
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2024-04-23 16:27:03 -0300

    Create sleepDebtCalculator.js

---
+++ 3. Functions/sleepDebtCalculator.js
@@ -0,0 +1,10 @@
+//LEARN JAVASCRIPT
+
+//Sleep Debt Calculator
+
+/*
+In this project, we’re going to practice functions in JavaScript so you can hone your skills
+and feel confident taking them to the real world. Why? Functions help us build separation of concerns so our
+code isn’t one long function.
+*/
+
